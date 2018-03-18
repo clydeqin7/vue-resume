eb81e5bd9cfecbbc74c0e51b5e45acdf3f9cddb5
clyde <clydeqin7@foxmail.com>
2018-03-18 10:48:39 +0800

    登录、注册、登出功能

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,10 @@ var app = new Vue({
         signInVisible: true,
         signUpVisible: false,
         loginVisible: false,
+        currentUser: {
+        id: undefined,
+        email: '',
+        },        
         resume:{
             name: '姓名1',
             gender: '男',
@@ -12,6 +16,14 @@ var app = new Vue({
             phone: '13800138001',
             email: 'email@example.com',
             jobTitle: '前端工程师',
+        },
+        signIn: {
+            email: '',
+            password: '',
+        },
+        signUp: {
+            email: '',
+            password: '',
         }
     },
     methods:{
@@ -19,10 +31,10 @@ var app = new Vue({
             this.resume[key] = value
         },
         onSaveClick(){
+            // TODO: 页面加载时是否判断用户登录了的
             let currentUser = AV.User.current()
-            console.log(currentUser)
             if (currentUser) {
-                // 跳转到首页
+                this.currentUser.id = currentUser.id
             }
             else {
                 this.showLogin()
@@ -30,6 +42,40 @@ var app = new Vue({
         },
         showLogin(){
             this.loginVisible = true
+        },
+        onLogout(){
+            AV.User.logOut()
+            this.currentUser.id = null
+        },
+        onSignIn(){
+            AV.User.logIn(this.signIn.email, this.signIn.password).then((loginedUser) => {
+                this.currentUser.id = loginedUser.id
+                this.currentUser.email = loginedUser.attributes.email
+                this.loginVisible = false
+            }, function (error) {
+                if(error.code === 210){
+                    alert('邮箱和密码不匹配')
+                }else if(erro.code === 211){
+                    alert('该邮箱未注册')
+                }
+            })           
+        },
+        onSignUp(){
+            const user = new AV.User()
+            user.setUsername(this.signUp.email)
+            user.setPassword(this.signUp.password)
+            user.setEmail(this.signUp.email)
+            user.signUp().then((user) => {
+                //TODO: 修改这个代码
+                AV.User.logOut()
+                alert('注册成功，将前往登录界面')
+                this.signUpVisible = false
+                this.signInVisible = true
+            }, (error) => {
+                if(error.code === 203){
+                    alert('此邮箱已经注册')
+                }
+            })            
         }
     }
 })
